f07080e59f02db85ddc32df533894b1ae6b469bb
Lukman Hossain <lukman.webdev@gmail.com>
2021-10-28 15:20:28 +0600

    final commit

--- src/Pages/KnowMore/KnowMore/KnowMore.js
+++ src/Pages/KnowMore/KnowMore/KnowMore.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import details from './FakeData';
 
 const KnowMore = () => {
     const { serviceid } = useParams();
+
+    const [detail, setDetail] = useState({});
+
+    useEffect(() => {
+        let detail = details.find(d => d.id == serviceid)
+        setDetail(detail);
+    }, [serviceid])
+
     return (
         <div>
-            <h2>This is know more: {serviceid}</h2>
+            <img src={detail?.img} alt="" />
+            <h3>{detail?.name}</h3>
+            <p>{detail?.description}</p>
         </div>
     );
 };
